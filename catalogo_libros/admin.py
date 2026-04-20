f12eb6db30172aac3194d22ca2a06e69ac5dcde4
Alcindo Chavarria <139635361+ByOneChav@users.noreply.github.com>
2026-04-20 11:33:00 -0400

    feat: agregar modelos Autor, Editora y Libro a catalogo_libro

--- catalogo_libros/admin.py
+++ catalogo_libros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from catalogo_libros.models import Autor, Editora, Libro
 
 # Register your models here.
+admin.site.register(Autor)
+admin.site.register(Editora)
+admin.site.register(Libro)
+
